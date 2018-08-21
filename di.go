3813176376b4ab507f5cc6b95a587034085a5924
Chris LaVoie <clavoie@gmail.com>
2018-08-21 09:10:08 -0400

    Forgot to add comment for the exported di func

--- di.go
+++ di.go
@@ -2,6 +2,8 @@ package jsonu
 
 import "github.com/clavoie/di"
 
+// NewDiDefs returns new dependency injection definitions
+// for this package.
 func NewDiDefs() []*di.Def {
 	return []*di.Def{
 		{NewSerializer, di.Singleton},
